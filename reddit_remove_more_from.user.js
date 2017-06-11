79773509ce6672682aecf52c059d494f9531698c
DR <daraeman@gmail.com>
2017-06-10 22:05:01 -0700

    fixed it not working

--- reddit_remove_more_from.user.js
+++ reddit_remove_more_from.user.js
@@ -4,8 +4,8 @@
 // @description  Removes the "more from subreddit" links that appear on comments pages when you are not signed in
 // @homepageURL  https://github.com/daraeman/reddit_remove_more_from
 // @author       daraeman
-// @version      1.0
-// @date         2017-03-08
+// @version      1.1
+// @date         2017-06-10
 // @include      https://www.reddit.com/r/*/comments/*
 // @downloadURL  https://github.com/daraeman/reddit_remove_more_from/raw/master/reddit_remove_more_from.user.js
 // @updateURL    https://github.com/daraeman/reddit_remove_more_from/raw/master/reddit_remove_more_from.meta.js
@@ -13,5 +13,7 @@
 
 (function() {
 	"use strict";
-	document.getElementsByClassName( "seo-comments-recommendations" ).remove();
+	var children = document.getElementsByClassName( "seo-comments-recommendations" );
+	for ( var i = 0; i < children.length; i++ )
+		children[i].parentNode.removeChild( children[i] );
 })();
