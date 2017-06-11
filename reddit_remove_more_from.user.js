64e7da351c6cc1ec4191fbc5c8b87f65a383228a
DR <daraeman@gmail.com>
2017-06-10 23:14:53 -0700

    remove the "more comments" button + text

--- reddit_remove_more_from.user.js
+++ reddit_remove_more_from.user.js
@@ -4,7 +4,7 @@
 // @description  Removes the "more from subreddit" links that appear on comments pages when you are not signed in
 // @homepageURL  https://github.com/daraeman/reddit_remove_more_from
 // @author       daraeman
-// @version      1.1
+// @version      1.2
 // @date         2017-06-10
 // @include      https://www.reddit.com/r/*/comments/*
 // @downloadURL  https://github.com/daraeman/reddit_remove_more_from/raw/master/reddit_remove_more_from.user.js
@@ -12,8 +12,23 @@
 // ==/UserScript==
 
 (function() {
-	"use strict";
-	var children = document.getElementsByClassName( "seo-comments-recommendations" );
-	for ( var i = 0; i < children.length; i++ )
-		children[i].parentNode.removeChild( children[i] );
+	var classes = [
+		"seo-comments-recommendations",
+		"seo-comments",
+	];
+	var ids = [
+		"bottom-comments",
+	];
+	var children = [];
+	classes.forEach( function( classname ){
+		children.push( document.getElementsByClassName( classname ) );
+	});
+	ids.forEach( function( id ){
+		children.push( [ document.getElementById( id ) ] );
+	});
+	for ( var a = 0; a < children.length; a++ ) {
+		for ( var b = 0; b < children[ a ].length; b++ ) {
+			children[a][b].parentNode.removeChild( children[a][b] );
+		}
+	}
 })();
